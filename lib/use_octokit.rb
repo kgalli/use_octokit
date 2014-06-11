183255cd723c55bcb405da9b635545810ac9b939
kgalli <mail@kgalli.de>
2014-06-11 11:03:08 +0200

    Add listing of tags

--- lib/use_octokit.rb
+++ lib/use_octokit.rb
@@ -2,15 +2,17 @@ require "use_octokit/version"
 require "octokit"
 
 module UseOctokit
-
   class RepositoryMananger
-
     def initialize(access_token)
       @client = Octokit::Client.new :access_token => access_token
     end
 
     def repositories
       @client.repos
+    end
+
+    def tags repo
+      @client.tags(repo)
     end
   end
 end
